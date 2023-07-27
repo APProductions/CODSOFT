fbe6b9477e4bb581da79968a7695de18a13e21ce
APProductions <129589641+APProductions@users.noreply.github.com>
2023-07-27 19:57:37 +0530

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,51 @@
+package Tasks;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+public class task2 {
+	
+	public static void filereader(String str) throws FileNotFoundException {
+		int count =0;
+		  File file = new File(str);
+	      FileInputStream fis = new FileInputStream(file);
+	      byte[] bytesArray = new byte[(int)file.length()];
+	      try {
+			fis.read(bytesArray);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	      String s = new String(bytesArray);
+	      String [] data = s.split(" ");
+	      for (int i=0; i<data.length; i++) {
+	         count++;
+	      }
+	      System.out.println("Number of Words in the given file are " +count);
+	   }
+	public static void stringreader(String str) {
+		int countWords=0;
+		countWords= str.split("").length;
+		System.out.println(countWords);
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		      
+		      Scanner sc=new Scanner(System.in);
+		      System.out.println("Enter String or File path");
+		      String str=sc.next();
+		      if (isFilePath(str)) {
+		    	  filereader(str);
+		        } else {
+		          stringreader(str);
+		        
+		        }
+		    }
+
+		    private static boolean isFilePath(String input) {
+		        File file = new File(input);
+		        return file.exists() && file.isFile();
+		    }
+		}
